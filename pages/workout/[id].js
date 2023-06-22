bd4cf57e650ce660fdd9cb5a9989858756f13eac
Hudson Pierce <hudsontpierce@gmail.com>
2023-06-22 14:56:30 -0400

    Add functionality to add exercise to workout

--- pages/workout/[id].js
+++ pages/workout/[id].js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import Layout from '../../components/layout/Layout';
 import ExerciseCard from '../../components/workouts/ExerciseCard';
+import ExerciseForm from '../../components/workouts/ExerciseForm';
 import CustomGrid from '../../components/common/CustomGrid';
 import { WORKOUTS } from '../../data/workouts';
+import Fab from '@mui/material/Fab';
+import { EXERCISES } from '../../data/exercises';
 
 export async function getServerSideProps(context) {
   const { id } = context.query;
@@ -23,11 +26,28 @@ export async function getServerSideProps(context) {
 
 export default function WorkoutPage({ workout }) {
   const [exercises, setExercises] = useState(workout.exercises);
+  const [exerciseFormOpen, setExerciseFormOpen] = useState(false);
+  const [exerciseName, setExerciseName] = useState(EXERCISES[0]);
+  const [exerciseReps, setExerciseReps] = useState(0);
+  const [exerciseSets, setExerciseSets] = useState(0);
+  const [exerciseWeight, setExerciseWeight] = useState(0);
 
   const deleteExercise = (exercise) => {
     const newExercises = exercises.filter(e => e.name !== exercise.name);
     setExercises(newExercises);
   };
+
+  const addExercise = () => {
+    const updatedExercise = {
+      id: Math.random().toString(),
+      name: exerciseName,
+      reps: exerciseReps,
+      sets: exerciseSets,
+      weight: exerciseWeight,
+    };
+    exercises.push(updatedExercise);
+    setExerciseFormOpen(false)
+  }
 
   const updateExercise = (updatedExercise) => {
     const updatedExercises = exercises.map(e => {
@@ -73,7 +93,29 @@ export default function WorkoutPage({ workout }) {
             updateExercise={updateExercise}
           />
         )}
-      />
+      /> 
+      <Fab variant='extended' color='success' onClick={() => setExerciseFormOpen(true)} sx={{ textTransform: 'none' }}>
+        Add Exercise
+      </Fab>
+      {/* <Fab variant='extended' color='primary' onClick={() => setExerciseFormOpen(true)} sx={{ textTransform: 'none' }}>
+        Save Workout
+      </Fab>
+      <Fab variant='extended' color='error' onClick={() => setExerciseFormOpen(true)} sx={{ textTransform: 'none' }}>
+        Delete Workout
+      </Fab> */}
+
+      {exerciseFormOpen && <ExerciseForm 
+        setExerciseName={setExerciseName}
+        setExerciseReps={setExerciseReps}
+        setExerciseSets={setExerciseSets}
+        setExerciseWeight={setExerciseWeight}
+        exerciseName={exerciseName}
+        exerciseReps={exerciseReps}
+        exerciseSets={exerciseSets}
+        exerciseWeight={exerciseWeight}
+        handleSaveChanges={addExercise}        
+        isModalOpen={exerciseFormOpen}
+        handleModalClose={() => setExerciseFormOpen(false)} />}
     </Layout>
   );
 }
